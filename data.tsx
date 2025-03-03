41f247b5b48d9d1984e7f94ee48c476fe9349517
hernanjorge.garrido <hernanjorge.garrido@gmail.com>
2025-03-03 07:16:48 +0000

    *fix LinkedIn url

--- data.tsx
+++ data.tsx
@@ -10,7 +10,7 @@ export const socialNetworks = [
   {
     id: 1,
     logo: <Linkedin size={30} strokeWidth={1} />,
-    src: "https://www.linkedin.com/ln/hernan-jorge-garrido",
+    src: "https://www.linkedin.com/in/hernan-jorge-garrido",
   },
   {
     id: 2,
